fe6b3da3bca9e547536863a072661e03c529babf
Blackmonk892 <blackmonk2224@gmail.com>
2026-01-18 18:02:41 +0530

    next tut- crud GET

---
+++ app/api/hello/route.js
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+
+let users = [
+  {
+    id: 1,
+    name: "Alice",
+    email: "alice@example.com",
+    age: 25,
+  },
+  {
+    id: 2,
+    name: "Bob",
+    email: "bob@example.com",
+    age: 30,
+  },
+  {
+    id: 3,
+    name: "Charlie",
+    email: "charlie@example.com",
+    age: 35,
+  },
+];
+export async function GET(request) {
+  try {
+    return NextResponse.json({
+      success: true,
+      data: users,
+      total: users.length,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: "Failed to fetch users",
+      },
+      { status: 500 },
+    );
+  }
+}
