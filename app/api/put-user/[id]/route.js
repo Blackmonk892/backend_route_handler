a5e6c006e2a7475e49d05ade9d195d50fc236763
Blackmonk892 <blackmonk2224@gmail.com>
2026-01-18 19:49:34 +0530

    made Put endpoint

---
+++ app/api/put-user/[id]/route.js
@@ -0,0 +1,54 @@
+import { NextResponse } from "next/server";
+import { users } from "../../hello/route";
+
+export async function PUT(request, { params }) {
+  try {
+    const { id } = await params;
+    const userId = parseInt(id);
+
+    const userIndex = users.findIndex((u) => u.id === userId);
+
+    if (userIndex === -1) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: "User not found",
+        },
+        { status: 404 },
+      );
+    }
+
+    const { name, email, age } = await request.json();
+
+    if (!name || !email || !age) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: "Missing required fields",
+        },
+        { status: 400 },
+      );
+    }
+
+    users[userIndex] = {
+      id: userId,
+      name: name,
+      email: email,
+      age: age,
+    };
+
+    return NextResponse.json({
+      success: true,
+      data: users[userIndex],
+      message: "User updated successfully",
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: "Failed to update user",
+      },
+      { status: 500 },
+    );
+  }
+}
