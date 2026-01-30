3fd795577b89b6549ccc3e23ea003e57610e5f93
Blackmonk892 <blackmonk2224@gmail.com>
2026-01-31 00:46:39 +0530

    delete endpoint

---
+++ app/api/delete/[id]/route.js
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import { users } from "../../hello/route";
+
+export async function DELETE(request, { params }) {
+  try {
+    const { id } = await params;
+    const userId = parseInt(id);
+    const userIndex = users.findIndex((u) => u.id === userId);
+    if (userIndex === -1) {
+      return NextResponse.json(
+        { success: false, error: "User not found" },
+        { status: 404 },
+      );
+    }
+
+    const deleteUser = users[userIndex];
+    users.splice(userIndex, 1);
+
+    return NextResponse.json({
+      success: true,
+      data: deleteUser,
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
